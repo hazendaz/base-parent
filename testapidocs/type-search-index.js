3a0104057e3ea8fc59993984752e7b1e6159f3d1
Jeremy Landis <jeremylandis@hotmail.com>
2021-10-14 21:51:33 -0400

    Wagon: Adding site to repository

---
+++ testapidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.googlecode.htmlcompressor.compressor","l":"HtmlCompressorTest"},{"p":"com.googlecode.htmlcompressor.compressor","l":"XmlCompressorTest"}]
